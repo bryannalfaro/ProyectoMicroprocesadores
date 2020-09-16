026cdf32bfa1e08feda82217333ef943d2b9755d
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-09-16 14:51:52 -0600

    elaboracion de txt a encriptar

--- proyecto.cpp
+++ proyecto.cpp
@@ -8,4 +8,48 @@ Julio Roberto Herrera
 Diego Alberto Alvarez
 */
 
+#include <iostream>
+#include <fstream>
 
+using namespace std;
+
+/*
+Metodo para elaborar archivo de texto
+*/
+void makeText(string opcion){
+	
+	ofstream documento;
+	documento.open(opcion.c_str(),ios::out);
+}
+
+int main(){
+	string op;
+	string menu;
+	
+
+	
+	printf("--------------------------------------\n");
+	cout<<"|  _                   _              |"<<endl;
+	cout<<"| /  ._   ._ _|_  _.  /   _   _|  _   |"<<endl;
+	cout<<"| |_ | |/ |_) |_ (_|  |_ (_) (_| (/_  |"<<endl;
+	cout<<"|      /  |                           |"<<endl;
+	printf("--------------------------------------\n");
+
+	printf("Programa de encriptacion automatizada\n");
+	printf("Escoge una de las siguientes opciones de trabajo: \n");
+	printf("1. Empezar trabajo de encriptacion\n");
+	printf("2. Salir\n");
+	cin>>menu;
+	
+	if(menu=="1"){
+		printf("Ingresa el nombre del archivo para guardar tus datos: ");
+		cin>>op;
+		makeText(op);
+	}else{
+		return 0;
+	}	
+	
+	
+	
+	
+}
