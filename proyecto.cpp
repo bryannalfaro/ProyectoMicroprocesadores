09eec7ee70fe6b75cc67c19cf02529525fe8c21a
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-09-16 13:57:03 -0600

    encabezado

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,1 +1,11 @@
+/*
+Universidad del valle de Guatemala
+Proyecto 2 - Programación de Microprocesadores
+Integrantes:
+Bryann Alfaro
+Diego de Jesus Arredondo
+Julio Roberto Herrera
+Diego Alberto Alvarez
+*/
+
 
